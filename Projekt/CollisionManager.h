5bcc9869d829b86fdc487d494ef2dd5de53d19ba
Secumfex <Arend.Buchacher@gmx.de>
2013-10-23 14:24:12 +0200

    WIP

--- Projekt/CollisionManager.h
+++ Projekt/CollisionManager.h
@@ -27,13 +27,27 @@ public:
 //---------------MEMBER VARIABLES------------
 private:
 	SceneManager* mSceneManager;
+	vector<Forces* > mCollisionForces;
+
 	RigidRocket* mRocket;
 	Vector3 collisionPoint;
 	float collisiontime;
 
-
 //---------------MEMBER FUNCTIONS------------
 public:
+	void update(float d_t);
+
+	//Teste alles gegen Alles
+	void collisionCheckAll();
+
+	//Teste
+	bool testObjectToObject();
+
+
+	//Wende alle Gefundenen CollisionsKräfte an und leere den KraftVektor
+	void applyAndEmptyCollisionForces();
+
+
 	void setScene(SceneManager* scene);
 	bool  isCollision ();
 	void collisionCheck(float d_t);
